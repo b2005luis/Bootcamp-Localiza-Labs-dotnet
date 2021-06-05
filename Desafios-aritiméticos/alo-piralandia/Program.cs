e6ce8072b4641bb0a876c4de55363aa30c64022f
Luis Alberto Batista Pedroso <b2005.luis@gmail.com>
2021-06-05 12:35:30 -0300

    Novo desafio adicionado e reorganização de pastas

---
+++ Desafios-aritiméticos/alo-piralandia/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace alo_piralandia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] verso = Console.ReadLine().ToCharArray();
+            Array.Reverse(verso);
+            string Reverse = String.Join("", verso);
+            Console.WriteLine(Reverse);
+        }
+    }
+}
